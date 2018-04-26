e6a173cd0cd09e7e06014dffc2e19a6ca1d32d3b
Astro <astro@spaceboyz.net>
2018-04-26 21:59:00 +0200

    port to embedded-hal, add some print debugging

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // Copyright 2017, Romuald Texier-Marcadé <romualdtm@gmail.com>
+//           2018, Astro <astro@spaceboyz.net>
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/license/LICENSE-2.0> or the MIT license
@@ -6,128 +7,22 @@
 // option.  This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![no_std]
 
-//! A pure Rust user space driver for NRF24L01(+) transceivers on Linux.
-//!
-//! The aim of this driver is to provide a rustic, easy to use, no non-sense
-//! API to drive an NRF24L01(+) transceiver.
-//!
-//! This is not a port another language, this driver has been written from scratch
-//! based on the device specs.
-//!
-//! For the moment, the driver only exposes an API for the most reliable communication
-//! scheme offered by NRF24L01 chips, that is _Enhanced Shockburst_ ™:
-//! automatic (hardware) packet acknowlegement with optional payload, dynamic payload length and
-//! long CRC (2 bytes).
-//!
-//! # Usage
-//!
-//! Add a dependency to `nrf24l01` to your `Cargo.toml`:
-//!
-//! ```toml
-//! [dependencies]
-//! nrf24l01 = "0.2.0"
-//! ```
-//!
-//! If you plan to compile for a Raspberry Pi, you may want to enable the `rpi_accel` feature for better
-//! performance. In that case, replace the above by:
-//!
-//! ```toml
-//! [dependencies.nrf24l01]
-//! version = "0.2.0"
-//! features = ["rpi_accel"]
-//! default-features = false
-//! ```
-//!
-//! # Examples
-//!
-//! ## Simple emitter
-//!
-//! ```rust,no_run
-//! extern crate nrf24l01;
-//!
-//! use std::time::Duration;
-//! use std::thread::sleep;
-//!
-//! use nrf24l01::{TXConfig, NRF24L01, PALevel, OperatingMode};
-//!
-//! fn main() {
-//!     let config = TXConfig {
-//!         channel: 108,
-//!         pa_level: PALevel::Low,
-//!         pipe0_address: *b"abcde",
-//!         max_retries: 3,
-//!         retry_delay: 2,
-//!         ..Default::default()
-//!     };
-//!     let mut device = NRF24L01::new(25, 0).unwrap();
-//!     let message = b"sendtest";
-//!     device.configure(&OperatingMode::TX(config)).unwrap();
-//!     device.flush_output().unwrap();
-//!     loop {
-//!         device.push(0, message).unwrap();
-//!         match device.send() {
-//!             Ok(retries) => println!("Message sent, {} retries needed", retries),
-//!             Err(err) => {
-//!                 println!("Destination unreachable: {:?}", err);
-//!                 device.flush_output().unwrap()
-//!             }
-//!         };
-//!         sleep(Duration::from_millis(5000));
-//!     }
-//! }
-//! ```
-//!
-//! ## Simple receiver listening to the simple emitter
-//!
-//! ```rust,no_run
-//! extern crate nrf24l01;
-//!
-//! use std::time::Duration;
-//! use std::thread::sleep;
-//!
-//! use nrf24l01::{RXConfig, NRF24L01, PALevel, OperatingMode};
-//!
-//! fn main() {
-//!     let config = RXConfig {
-//!         channel: 108,
-//!         pa_level: PALevel::Low,
-//!         pipe0_address: *b"abcde",
-//!         ..Default::default()
-//!     };
-//!     let mut device = NRF24L01::new(25, 0).unwrap();
-//!     device.configure(&OperatingMode::RX(config)).unwrap();
-//!     device.listen().unwrap();
-//!     loop {
-//!         sleep(Duration::from_millis(500));
-//!         if device.data_available().unwrap() {
-//!             device
-//!                 .read_all(|packet| {
-//!                     println!("Received {:?} bytes", packet.len());
-//!                     println!("Payload {:?}", packet);
-//!                 })
-//!                 .unwrap();
-//!         }
-//!     }
-//! }
-//! ```
+#[macro_use(block)]
+extern crate nb;
+extern crate embedded_hal;
+// TODO:
+extern crate cortex_m_semihosting;
 
+use core::fmt::Debug;
+use embedded_hal::digital::OutputPin;
+use embedded_hal::spi;
+use embedded_hal::blocking::spi::Transfer as SpiTransfer;
+use embedded_hal::blocking::delay::DelayUs;
 
-extern crate spidev;
-#[cfg(feature = "rpi_accel")]
-mod rpi_ce;
-#[cfg(not(feature = "rpi_accel"))]
-mod sysfs_ce;
-
-use std::io;
-use std::thread::sleep;
-use std::time::Duration;
-
-#[cfg(feature = "rpi_accel")]
-use rpi_ce::CEPin;
-#[cfg(not(feature = "rpi_accel"))]
-use sysfs_ce::CEPin;
-
+use core::fmt::Write;
+use cortex_m_semihosting::hio;
 
 /// Supported air data rates.
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -318,29 +213,59 @@ const DYNPD: Register = 0x1C;
 const FEATURE: Register = 0x1D;
 
 
+#[derive(Debug)]
+pub enum Error<SPIE: Debug> {
+    SpiError(SPIE),
+}
+
+impl<SPIE: Debug> From<SPIE> for Error<SPIE> {
+    fn from(e: SPIE) -> Self {
+        Error::SpiError(e)
+    }
+}
 
 /// The driver
-pub struct NRF24L01 {
-    ce: CEPin,
-    spi: spidev::Spidev,
+pub struct NRF24L01<CE: OutputPin, SPI: SpiTransfer<u8>> {
+    ce: CE,
+    spi: SPI,
     base_config: u8,
 }
 
-impl NRF24L01 {
-    // Private methods and functions
+pub mod setup {
+    use embedded_hal::spi;
 
-    fn send_command(&self, data_out: &[u8], data_in: &mut [u8]) -> io::Result<()> {
-        let mut transfer = spidev::SpidevTransfer::read_write(data_out, data_in);
-        self.spi.transfer(&mut transfer)
+    /// Setup parameters
+    pub fn spi_mode() -> spi::Mode {
+        spi::Mode {
+            polarity: spi::Polarity::IdleLow,
+            phase: spi::Phase::CaptureOnFirstTransition,
+        }
     }
 
-    fn write_register(&self, register: Register, byte: u8) -> io::Result<()> {
+    pub fn clock_mhz() -> u32 {
+        10
+    }
+}
+
+impl<CE: OutputPin, SPI: SpiTransfer<u8, Error=SPIE>, SPIE: Debug> NRF24L01<CE, SPI> {
+    // Private methods and functions
+
+    fn send_command(&mut self, data_out: &[u8], data_in: &mut [u8]) -> Result<(), Error<SPIE>> {
+        // let mut stdout = hio::hstdout().unwrap();
+        // writeln!(stdout, "send {:?}", data_out).unwrap();
+        data_in.copy_from_slice(data_out);
+        self.spi.transfer(data_in)?;
+        // writeln!(stdout, "recvd {:?}", data_in).unwrap();
+        Ok(())
+    }
+
+    fn write_register(&mut self, register: Register, byte: u8) -> Result<(), Error<SPIE>> {
         // For single byte registers only
         let mut response_buffer = [0u8; 2];
         self.send_command(&[W_REGISTER | register, byte], &mut response_buffer)
     }
 
-    fn read_register(&self, register: Register) -> io::Result<(u8, u8)> {
+    fn read_register(&mut self, register: Register) -> Result<(u8, u8), Error<SPIE>> {
         // For single byte registers only.
         // Return (STATUS, register)
         let mut response_buffer = [0u8; 2];
@@ -351,7 +276,7 @@ impl NRF24L01 {
         Ok((response_buffer[0], response_buffer[1]))
     }
 
-    fn setup_rf(&self, rate: DataRate, level: PALevel) -> io::Result<()> {
+    fn setup_rf(&mut self, rate: DataRate, level: PALevel) -> Result<(), Error<SPIE>> {
         let rate_bits: u8 = match rate {
             DataRate::R250Kbps => 0b0010_0000,
             DataRate::R1Mbps => 0,
@@ -366,7 +291,7 @@ impl NRF24L01 {
         self.write_register(RF_SETUP, rate_bits | level_bits)
     }
 
-    fn set_channel(&self, channel: u8) -> io::Result<()> {
+    fn set_channel(&mut self, channel: u8) -> Result<(), Error<SPIE>> {
         if channel < 126 {
             self.write_register(RF_CH, channel)
         } else {
@@ -374,14 +299,14 @@ impl NRF24L01 {
         }
     }
 
-    fn set_full_address(&self, pipe: Register, address: [u8; 5]) -> io::Result<()> {
+    fn set_full_address(&mut self, pipe: Register, address: [u8; 5]) -> Result<(), Error<SPIE>> {
         let mut response_buffer = [0u8; 6];
         let mut command = [W_REGISTER | pipe, 0, 0, 0, 0, 0];
         command[1..].copy_from_slice(&address);
         self.send_command(&command, &mut response_buffer)
     }
 
-    fn configure_receiver(&self, config: &RXConfig) -> io::Result<u8> {
+    fn configure_receiver(&mut self, config: &RXConfig) -> Result<u8, Error<SPIE>> {
         // set data rate
         // set PA level
         self.setup_rf(config.data_rate, config.pa_level)?;
@@ -422,7 +347,7 @@ impl NRF24L01 {
         Ok(0b0011_1101)
     }
 
-    fn configure_transmitter(&self, config: &TXConfig) -> io::Result<u8> {
+    fn configure_transmitter(&mut self, config: &TXConfig) -> Result<u8, Error<SPIE>> {
         // set data rate
         // set PA level
         self.setup_rf(config.data_rate, config.pa_level)?;
@@ -457,24 +382,18 @@ impl NRF24L01 {
 
     /// Construct a new driver instance.
     ///
-    /// * `ce_pin`: the GPIO number (Linux SysFS) connected to the CE pin of the transceiver
-    /// * `spi_device`: the SPI device number (or channel) the transceiver is connected to.
-    ///
-    /// We use the spidev linux kernel driver. Ensure you have enabled SPI on your system.
-    ///
     /// # Errors
     ///
     /// System IO errors
     ///
-    pub fn new(ce_pin: u64, spi_device: u8) -> io::Result<NRF24L01> {
-        let mut spi = try!(spidev::Spidev::open(format!("/dev/spidev0.{}", spi_device)));
-        let options = spidev::SpidevOptions::new()
-            .bits_per_word(8)
-            .max_speed_hz(10_000_000)
-            .mode(spidev::SPI_MODE_0)
-            .build();
-        spi.configure(&options)?;
-        let ce = CEPin::new(ce_pin)?;
+    pub fn new(ce: CE, spi: SPI) -> Result<Self, Error<SPIE>> {
+        // TODO
+        // let options = spidev::SpidevOptions::new()
+        //     .bits_per_word(8)
+        //     .max_speed_hz(10_000_000)
+        //     .mode(spidev::SPI_MODE_0)
+        //     .build();
+        // spi.configure(&options)?;
         Ok(NRF24L01 {
             ce,
             spi,
@@ -490,8 +409,8 @@ impl NRF24L01 {
     ///
     /// All commands work when the device is in standby (recommended) as well as
     /// active state.
-    pub fn configure(&mut self, mode: &OperatingMode) -> io::Result<()> {
-        self.ce.down()?;
+    pub fn configure(&mut self, mode: &OperatingMode) -> Result<(), Error<SPIE>> {
+        self.ce.set_low();
         // auto acknowlegement
         self.write_register(EN_AA, 0b0011_1111)?;
         // dynamic payload and payload with ACK
@@ -517,22 +436,24 @@ impl NRF24L01 {
     ///
     /// The power consumption is minimum in this mode, and the device ceases all operation.
     /// It only accepts configuration commands.
-    pub fn power_down(&mut self) -> io::Result<()> {
-        self.ce.down()?;
-        self.write_register(CONFIG, self.base_config)
+    pub fn power_down(&mut self) -> Result<(), Error<SPIE>> {
+        self.ce.set_low();
+        let base_config = self.base_config;
+        self.write_register(CONFIG, base_config)
     }
 
     /// Power the device up for full operation.
-    pub fn power_up(&self) -> io::Result<()> {
-        self.write_register(CONFIG, self.base_config | 0b0000_0010)
+    pub fn power_up(&mut self) -> Result<(), Error<SPIE>> {
+        let base_config = self.base_config | 0b0000_0110;
+        self.write_register(CONFIG, base_config)
     }
 
     /// Put the device in standby (RX Mode)
     ///
     /// Only used in RX mode to suspend active listening.
     /// In TX mode, standby is the default state when not sending data.
-    pub fn standby(&mut self) -> io::Result<()> {
-        self.ce.down()?; // always returnss without error.
+    pub fn standby(&mut self) -> Result<(), Error<SPIE>> {
+        self.ce.set_low(); // always returnss without error.
         Ok(())
     }
 
@@ -540,9 +461,9 @@ impl NRF24L01 {
     ///
     /// In RX mode, call this function after a `.configure(...)`, `.standby()` or `power_up()` to
     /// accept incoming packets.
-    pub fn listen(&mut self) -> io::Result<()> {
+    pub fn listen(&mut self) -> Result<(), Error<SPIE>> {
         if self.is_receiver() {
-            self.ce.up()?;
+            self.ce.set_high();
         }
         Ok(())
     }
@@ -552,7 +473,7 @@ impl NRF24L01 {
     ///
     /// Works in both RX and TX modes. In TX mode, this function returns true if
     /// a ACK payload has been received.
-    pub fn data_available(&self) -> io::Result<bool> {
+    pub fn data_available(&mut self) -> Result<bool, Error<SPIE>> {
         self.read_register(FIFO_STATUS).and_then(
             |(_, fifo_status)| {
                 Ok(fifo_status.trailing_zeros() >= 1)
@@ -570,7 +491,7 @@ impl NRF24L01 {
     /// **Note**: this function puts the device in standby mode during
     /// the processing of the queue and restores operations when it returns *successfully*.
     /// So the `process_packet` callback should better return quickly.
-    pub fn read_all<F>(&mut self, mut process_packet: F) -> io::Result<u8>
+    pub fn read_all<F>(&mut self, mut process_packet: F) -> Result<u8, Error<SPIE>>
     where
         F: FnMut(&[u8]) -> (),
     {
@@ -581,16 +502,16 @@ impl NRF24L01 {
         // message counter
         let mut count = 0u8;
         // save CE state
-        self.ce.save_state();
+        let ce_state = self.ce.is_high();
         // Standby
-        self.ce.down()?;
+        self.ce.set_low();
         // process queue
         while self.data_available()? {
             self.send_command(&[R_RX_PL_WID, 0], &mut pl_wd)?;
             let width = pl_wd[1] as usize;
             if width != 0 {
                 // can it be false?
-                let ubound = width + 1;
+                let ubound = (width + 1).min(33);
                 self.send_command(
                     &out_buffer[..ubound],
                     &mut receive_buffer[..ubound],
@@ -602,7 +523,11 @@ impl NRF24L01 {
         // Clear interrupt
         self.write_register(STATUS, 0b0100_0000)?;
         // Restore previous CE state
-        self.ce.restore_state()?;
+        if ce_state {
+            self.ce.set_high();
+        } else {
+            self.ce.set_low();
+        }
         Ok(count)
     }
 
@@ -630,14 +555,12 @@ impl NRF24L01 {
     /// it receives a new, different message from the same transmitter.
     /// So, it keeps the ACK payload under hand in case the transmitter resends the same
     /// packet over again.
-    pub fn push(&self, pipe_num: u8, data: &[u8]) -> io::Result<()> {
+    pub fn push(&mut self, pipe_num: u8, data: &[u8]) -> Result<(), Error<SPIE>> {
         let (status, fifo_status) = self.read_register(FIFO_STATUS)?;
         if (status & 1 != 0) || (fifo_status & 0b0010_0000 != 0) {
             // TX_FIFO is full
-            Err(io::Error::new(
-                io::ErrorKind::WriteZero,
-                "Sending queue is full!",
-            ))
+            // TODO
+            panic!("Sending queue is full!")
         } else {
             let command = if self.is_receiver() {
                 let actual_pipe_num: u8 = if pipe_num < 6 { pipe_num } else { 5 };
@@ -646,10 +569,8 @@ impl NRF24L01 {
                 W_TX_PAYLOAD
             };
             if data.len() > 32 {
-                Err(io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    "Packet too big!",
-                ))
+                // TODO
+                panic!("Packet too big!")
             } else {
                 let mut out_buffer = [command; 33];
                 let ubound = data.len() + 1;
@@ -669,11 +590,7 @@ impl NRF24L01 {
     /// The payloads that failed to be sent remain in the TX queue.
     /// You can call `.send()` again to relaunch a send/retry cycle or
     /// call `.flush_output` to clear the queue.
-    ///
-    /// # Errors
-    /// Return Spidev errors as well as a custom io::ErrorKind::Timeout
-    /// when the maximun number of retries has been reached.
-    pub fn send(&mut self) -> io::Result<u8> {
+    pub fn send<D: DelayUs<u16>>(&mut self, d: &mut D) -> Result<u8, Error<SPIE>> {
         // clear TX_DS and MAX_RT
         self.write_register(STATUS, 0x30)?;
         // init retry counter
@@ -682,15 +599,15 @@ impl NRF24L01 {
         let mut packets_left = fifo_status & 0x10 == 0;
         while packets_left {
             // send with a 10us pulse
-            self.ce.up()?;
-            sleep(Duration::new(0, 10_000));
-            self.ce.down()?;
+            self.ce.set_high();
+            d.delay_us(10);
+            self.ce.set_low();
             let mut status = 0u8;
             let mut observe = 0u8;
             // wait for ACK
             while status & 0x30 == 0 {
                 // wait at least 500us
-                sleep(Duration::new(0, 500_000));
+                d.delay_us(500);
                 let outcome = self.read_register(OBSERVE_TX)?;
                 status = outcome.0;
                 observe = outcome.1;
@@ -701,10 +618,7 @@ impl NRF24L01 {
                 // clear MAX_RT
                 self.write_register(STATUS, 0x10)?;
                 // force return
-                return Err(io::Error::new(
-                    io::ErrorKind::TimedOut,
-                    "Maximum number of retries reached!",
-                ));
+                panic!("Maximum number of retries reached!");
             };
             // Success
             // clear TX_DS
@@ -720,7 +634,7 @@ impl NRF24L01 {
     /// Clear input queue.
     ///
     /// In RX mode, use only when device is in standby.
-    pub fn flush_input(&self) -> io::Result<()> {
+    pub fn flush_input(&mut self) -> Result<(), Error<SPIE>> {
         let mut buffer = [0u8];
         self.send_command(&[FLUSH_RX], &mut buffer)?;
         Ok(())
@@ -729,7 +643,7 @@ impl NRF24L01 {
     /// Clear output queue.
     ///
     /// In RX mode, use only when device is in standby.
-    pub fn flush_output(&self) -> io::Result<()> {
+    pub fn flush_output(&mut self) -> Result<(), Error<SPIE>> {
         let mut buffer = [0u8];
         self.send_command(&[FLUSH_TX], &mut buffer)?;
         Ok(())
