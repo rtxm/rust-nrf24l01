bf6639c8c8ae3984c3c59023a0b07fd6fc3ad995
Astro <astro@spaceboyz.net>
2018-05-06 00:34:33 +0200

    Device.update_register()

--- src/device.rs
+++ src/device.rs
@@ -28,6 +28,24 @@ pub trait Device {
     fn write_register<R: Register>(&mut self, register: R) -> Result<Status, Self::Error>;
     fn read_register<R: Register>(&mut self) -> Result<(Status, R), Self::Error>;
 
+    fn update_register<Reg, F, R>(&mut self, f: F) -> Result<R, Self::Error>
+    where
+        Reg: Register + PartialEq + Clone,
+        F: FnOnce(&mut Reg) -> R,
+    {
+        // Use `update_config()` for `registers::Config`
+        assert!(Reg::addr() != 0x00);
+
+        let (_, old_register) = self.read_register::<Reg>()?;
+        let mut register = old_register.clone();
+        let result = f(&mut register);
+
+        if register != old_register {
+            self.write_register(register)?;
+        }
+        Ok(result)
+    }
+
     fn update_config<F, R>(&mut self, f: F) -> Result<R, Self::Error>
         where F: FnOnce(&mut Config) -> R;
 }
