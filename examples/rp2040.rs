60cc438520ab6a3bd8ae06383fc48fa15e15f748
Brandon Ros <brandonros1@gmail.com>
2023-07-24 19:18:21 -0400

    update example to support/clarify CE usage

--- examples/rp2040.rs
+++ examples/rp2040.rs
@@ -2,26 +2,25 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use embedded_hal::digital::OutputPin;
+use nrf24l01::{TXConfig, PALevel, NRF24L01, OperatingMode, RXConfig};
 use panic_probe as _;
 use defmt_rtt as _;
 
 use core::cell::RefCell;
 
-use nrf24l01::{TXConfig, PALevel, NRF24L01, OperatingMode};
 use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_sync::blocking_mutex::{Mutex, raw::NoopRawMutex};
 use embassy_executor::Spawner;
 use embassy_rp::{
-    spi::Spi,
-    gpio::{Level, Output}, spi::Blocking
+    spi::{Spi, Blocking},
+    gpio::{Level, Output, Pin},
 };
 use embassy_time::{Duration, Timer};
 
-async fn led_blink<T>(mut output_pin: T) where T: OutputPin {
-    output_pin.set_high();
+async fn blink<T>(led: &mut Output<'_, T>) where T: Pin {
+    led.set_high();
     Timer::after(Duration::from_millis(100)).await;
-    output_pin.set_low();
+    led.set_low();
     Timer::after(Duration::from_millis(100)).await;
 }
 
@@ -32,21 +31,36 @@ async fn main(_spawner: Spawner) {
     let peripherals = embassy_rp::init(peripherals_config);
     // LED
     let mut led_output = Output::new(peripherals.PIN_25, Level::Low);
-    // SPI
-    let mut spi_config = embassy_rp::spi::Config::default();
-    spi_config.frequency = 10_000_000; // TODO: is this right?
-    spi_config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition; // TODO: is this right?
-    spi_config.polarity = embassy_rp::spi::Polarity::IdleHigh; // TODO: is this right?
-    let clk = peripherals.PIN_10;
-    let mosi = peripherals.PIN_11;
-    let miso =  peripherals.PIN_12;
-    let cs = peripherals.PIN_9;
-    let cs_output = Output::new(cs, Level::High);
-    let spi: Spi<'_, _, Blocking> = Spi::new_blocking(peripherals.SPI1, clk, mosi, miso, spi_config.clone());
-    let spi_bus: Mutex<NoopRawMutex, _> = Mutex::new(RefCell::new(spi));
-    let spi_device = SpiDeviceWithConfig::new(&spi_bus, cs_output, spi_config);
-    // NRF24L01
-    let nrf24l01_config = TXConfig {
+    // SPI0
+    let mut spi0_config = embassy_rp::spi::Config::default();
+    spi0_config.frequency = 10_000_000; // TODO: is this right?
+    spi0_config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition; // TODO: is this right?
+    spi0_config.polarity = embassy_rp::spi::Polarity::IdleHigh; // TODO: is this right?
+    let spi0_clk = peripherals.PIN_18;
+    let spi0_mosi = peripherals.PIN_19;
+    let spi0_miso =  peripherals.PIN_20;
+    let spi0_cs = peripherals.PIN_21;
+    let spi0_cs_output = Output::new(spi0_cs, Level::High);
+    let spi0: Spi<'_, _, Blocking> = Spi::new_blocking(peripherals.SPI0, spi0_clk, spi0_mosi, spi0_miso, spi0_config.clone());
+    let spi0_bus: Mutex<NoopRawMutex, _> = Mutex::new(RefCell::new(spi0));
+    let spi0_device = SpiDeviceWithConfig::new(&spi0_bus, spi0_cs_output, spi0_config);
+    // SPI1
+    let mut spi1_config = embassy_rp::spi::Config::default();
+    spi1_config.frequency = 10_000_000; // TODO: is this right?
+    spi1_config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition; // TODO: is this right?
+    spi1_config.polarity = embassy_rp::spi::Polarity::IdleHigh; // TODO: is this right?
+    let sp1_clk = peripherals.PIN_10;
+    let sp1_mosi = peripherals.PIN_11;
+    let sp1_miso =  peripherals.PIN_12;
+    let sp1_cs = peripherals.PIN_13;
+    let sp1_cs_output = Output::new(sp1_cs, Level::High);
+    let spi1: Spi<'_, _, Blocking> = Spi::new_blocking(peripherals.SPI1, sp1_clk, sp1_mosi, sp1_miso, spi1_config.clone());
+    let spi1_bus: Mutex<NoopRawMutex, _> = Mutex::new(RefCell::new(spi1));
+    let spi1_device = SpiDeviceWithConfig::new(&spi1_bus, sp1_cs_output, spi1_config);
+    // NRF24L01P transmitter
+    let tx_ce = peripherals.PIN_0;
+    let tx_ce_output = Output::new(tx_ce, Level::Low);
+    let tx_config = TXConfig {
         channel: 108,
         pa_level: PALevel::Low,
         pipe0_address: *b"abcde",
@@ -54,21 +68,44 @@ async fn main(_spawner: Spawner) {
         retry_delay: 2,
         ..Default::default()
     };
-    let mut device = NRF24L01::new(spi_device, cs_output).unwrap(); // TODO: how to share cs_output with SpiDeviceWithConfig because it gets moved?
+    let mut nrf24l01_tx_device = NRF24L01::new(spi0_device, tx_ce_output).unwrap();
     let message = b"sendtest";
-    device.configure(&OperatingMode::TX(nrf24l01_config)).unwrap();
-    device.flush_output().unwrap();
+    nrf24l01_tx_device.configure(&OperatingMode::TX(tx_config)).unwrap();
+    nrf24l01_tx_device.flush_output().unwrap();
+    // NRF24L01P receiver
+    let rx_ce = peripherals.PIN_22;
+    let rx_ce_output = Output::new(rx_ce, Level::Low);
+    let rx_config = RXConfig {
+        channel: 108,
+        pa_level: PALevel::Low,
+        pipe0_address: *b"abcde",
+        ..Default::default()
+    };
+    let mut nrf24l01_rx_device = NRF24L01::new(spi1_device, rx_ce_output).unwrap();
+    nrf24l01_rx_device.configure(&OperatingMode::RX(rx_config)).unwrap();
+    nrf24l01_rx_device.listen().unwrap();
     loop {
-        led_blink(&mut led_output).await;
-        // spi
-        device.push(0, message).unwrap();
-        match device.send() {
+        // blink
+        blink(&mut led_output).await;
+        // transmit
+        nrf24l01_tx_device.push(0, message).unwrap();
+        match nrf24l01_tx_device.send() {
             Ok(retries) => defmt::info!("Message sent, {} retries needed", retries),
             Err(_err) => {
-                defmt::error!("Destination unreachable"); // TODO: print/format err somehow?
-                device.flush_output().unwrap()
+                defmt::error!("Destination unreachable"); // TODO: fmt error?
+                nrf24l01_tx_device.flush_output().unwrap()
             }
         };
-        Timer::after(Duration::from_millis(5000)).await;
+        // receive
+        if nrf24l01_rx_device.data_available().unwrap() {
+            nrf24l01_rx_device
+                .read_all(|packet| {
+                    defmt::info!("Received {:?} bytes", packet.len());
+                    defmt::info!("Payload {:?}", packet);
+                })
+                .unwrap();
+        }
+        // sleep
+        Timer::after(Duration::from_millis(1000)).await;
     }
 }
