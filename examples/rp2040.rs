7bf4e538d128e60939dd1b6d177ff4f8355af28a
Brandon Ros <brandonros1@gmail.com>
2023-07-24 19:24:24 -0400

    needed an allocator?

--- examples/rp2040.rs
+++ examples/rp2040.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use embedded_alloc::Heap;
 use nrf24l01::{TXConfig, PALevel, NRF24L01, OperatingMode, RXConfig};
 use panic_probe as _;
 use defmt_rtt as _;
@@ -16,6 +17,9 @@ use embassy_rp::{
     gpio::{Level, Output, Pin},
 };
 use embassy_time::{Duration, Timer};
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
 
 async fn blink<T>(led: &mut Output<'_, T>) where T: Pin {
     led.set_high();
@@ -32,33 +36,34 @@ async fn main(_spawner: Spawner) {
     // LED
     let mut led_output = Output::new(peripherals.PIN_25, Level::Low);
     // SPI0
-    let mut spi0_config = embassy_rp::spi::Config::default();
-    spi0_config.frequency = 10_000_000; // TODO: is this right?
-    spi0_config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition; // TODO: is this right?
-    spi0_config.polarity = embassy_rp::spi::Polarity::IdleHigh; // TODO: is this right?
     let spi0_clk = peripherals.PIN_18;
     let spi0_mosi = peripherals.PIN_19;
     let spi0_miso =  peripherals.PIN_20;
     let spi0_cs = peripherals.PIN_21;
+    let tx_ce = peripherals.PIN_22;
+    let mut spi0_config = embassy_rp::spi::Config::default();
+    spi0_config.frequency = 10_000_000; // TODO: is this right?
+    spi0_config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition; // TODO: is this right?
+    spi0_config.polarity = embassy_rp::spi::Polarity::IdleHigh; // TODO: is this right?
     let spi0_cs_output = Output::new(spi0_cs, Level::High);
     let spi0: Spi<'_, _, Blocking> = Spi::new_blocking(peripherals.SPI0, spi0_clk, spi0_mosi, spi0_miso, spi0_config.clone());
     let spi0_bus: Mutex<NoopRawMutex, _> = Mutex::new(RefCell::new(spi0));
     let spi0_device = SpiDeviceWithConfig::new(&spi0_bus, spi0_cs_output, spi0_config);
     // SPI1
+    let spi1_clk = peripherals.PIN_10;
+    let spi1_mosi = peripherals.PIN_11;
+    let spi1_miso =  peripherals.PIN_12;
+    let spi1_cs = peripherals.PIN_13;
+    let rx_ce = peripherals.PIN_9;
     let mut spi1_config = embassy_rp::spi::Config::default();
     spi1_config.frequency = 10_000_000; // TODO: is this right?
     spi1_config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition; // TODO: is this right?
     spi1_config.polarity = embassy_rp::spi::Polarity::IdleHigh; // TODO: is this right?
-    let sp1_clk = peripherals.PIN_10;
-    let sp1_mosi = peripherals.PIN_11;
-    let sp1_miso =  peripherals.PIN_12;
-    let sp1_cs = peripherals.PIN_13;
-    let sp1_cs_output = Output::new(sp1_cs, Level::High);
-    let spi1: Spi<'_, _, Blocking> = Spi::new_blocking(peripherals.SPI1, sp1_clk, sp1_mosi, sp1_miso, spi1_config.clone());
+    let sp1_cs_output = Output::new(spi1_cs, Level::High);
+    let spi1: Spi<'_, _, Blocking> = Spi::new_blocking(peripherals.SPI1, spi1_clk, spi1_mosi, spi1_miso, spi1_config.clone());
     let spi1_bus: Mutex<NoopRawMutex, _> = Mutex::new(RefCell::new(spi1));
     let spi1_device = SpiDeviceWithConfig::new(&spi1_bus, sp1_cs_output, spi1_config);
-    // NRF24L01P transmitter
-    let tx_ce = peripherals.PIN_0;
+    // NRF24L01P transmitter (SPI0)
     let tx_ce_output = Output::new(tx_ce, Level::Low);
     let tx_config = TXConfig {
         channel: 108,
@@ -72,8 +77,7 @@ async fn main(_spawner: Spawner) {
     let message = b"sendtest";
     nrf24l01_tx_device.configure(&OperatingMode::TX(tx_config)).unwrap();
     nrf24l01_tx_device.flush_output().unwrap();
-    // NRF24L01P receiver
-    let rx_ce = peripherals.PIN_22;
+    // NRF24L01P receiver (SPI1)
     let rx_ce_output = Output::new(rx_ce, Level::Low);
     let rx_config = RXConfig {
         channel: 108,
